db0e0544edcbf1efe88ea77db6ec8e02b8367c38
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2026-07-23 11:28:29 -0400

    Add initial USB CDC middleware config

--- firmware/Core/Inc/stm32l4xx_it.h
+++ firmware/Core/Inc/stm32l4xx_it.h
@@ -59,6 +59,7 @@ void DMA1_Channel1_IRQHandler(void);
 void DMA1_Channel2_IRQHandler(void);
 void DMA1_Channel3_IRQHandler(void);
 void DMA1_Channel5_IRQHandler(void);
+void USB_IRQHandler(void);
 void QUADSPI_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
